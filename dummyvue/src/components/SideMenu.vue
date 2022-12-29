3d9ce2a9ba78cdf32ce1e643dc26e13dccd4fcbd
Jacaroni n' Cheese <agentjtoo7@aol.com>
2022-12-29 07:22:33 -0500

    SideMenu routing

--- dummyvue/src/components/SideMenu.vue
+++ dummyvue/src/components/SideMenu.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-
+    import router from "@/router"
 </script>
 
 <template>
-    <div class="box has-background-warning">
-        this is SideMenu
+    <div class="box has-background-white">
+        <aside class="menu">
+            <p class="menu-label">
+                Pages
+            </p>
+            <ul class="menu-list">
+                <li><a :class="{'is-active': router.currentRoute.value.name== 'home'}" @click="router.push('/')">Home</a></li>
+                <li><a :class="{'is-active': router.currentRoute.value.name== 'about'}" @click="router.push('/about')">About</a></li>
+            </ul>
+        </aside>
     </div>
 </template>
