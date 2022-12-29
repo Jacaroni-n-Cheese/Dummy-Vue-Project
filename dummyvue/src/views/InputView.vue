e0b5de85c5ac34946470c9f879bae268c50f4120
Jacaroni n' Cheese <agentjtoo7@aol.com>
2022-12-29 13:27:02 -0500

    changing inputview location

--- dummyvue/src/views/InputView.vue
+++ dummyvue/src/views/InputView.vue
@@ -7,9 +7,10 @@
 <!-- https://bulma.io/documentation/helpers/color-helpers/ -->
 <template>
   <div class="box">
-    <div class="box">
-      {{textboxtext}}
-    </div>
     <input class="input" type="text" placeholder="input" v-model="textboxtext">
   </div>
+
+  <div class="box">
+      {{textboxtext}}
+    </div>
 </template>
